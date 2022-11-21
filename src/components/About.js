66a102c7cae4ca407ef3efb1043b598a74261334
Holly Hoffman <hoffh5334@gmail.com>
2022-11-21 10:28:48 -0600

    added about section

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function About() {
+  return (
+    <section class="about">
+      <h1 id="about">Hi! I'm Holly.</h1>
+      <p>I'm a future Full Stack Web Developer currently attending the University of Minnesota's Full Stack Web Dev Boot Camp. </p>
+      <p>When I'm not busy with boot camp and gaining knowledge in software development, I'm usually hanging out with my four dogs or training my youngest dog to hunt.</p>
+
+    </section>
+  );
+}
+
+export default About;
